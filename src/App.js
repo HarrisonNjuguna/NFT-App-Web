7bb8881ce9fbc5bb98505f3b30733bd1c3f666d8
HarrisonNjuguna <machariaharry12@gmail.com>
2022-09-17 16:18:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from "./components";
+import styles from "./styles/Global";
+import assets from "./assets";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper 
+        title="Your local Kenya store of awesome NFTs. Start selling and growing."
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join over 2 million Kenyans using ProNef Marketplace."
+
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+
+      <SectionWrapper 
+        title="Beautiful & Simple User Interface"
+        description="NFT purchase made easier and simple. Smooth UI experience from ProNef Kenya Market place."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+
+      <Features />
+
+      <SectionWrapper 
+        title="Deployment"
+        description="ProNef Kenya is built using React Native and Expo CLI which runs natively on all user devices."
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper 
+        title="Creative, simple minimalistic UI"
+        description="The app contains only two screens. The home screen contains the listing of the NFTs and details screen contains details of each NFT and the number of people bidding."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Created by {" "}
+        <span className="bold">Harry.dev 💻 </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
